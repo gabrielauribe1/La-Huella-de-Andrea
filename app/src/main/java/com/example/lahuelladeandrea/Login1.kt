a65e7ca89c8a9863d20f0c289a2cd12a425f3f5d
CesarArellano <raywayday@gmail.com>
2021-11-19 15:18:53 -0600

    Adding DB with Room and adding register and login functions

--- app/src/main/java/com/example/lahuelladeandrea/Login1.kt
+++ app/src/main/java/com/example/lahuelladeandrea/Login1.kt
@@ -4,19 +4,61 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.lahuelladeandrea.database.AppDatabase
+import com.example.lahuelladeandrea.database.UserModel
+import kotlinx.coroutines.*
 
 class Login1 : AppCompatActivity() {
+    private lateinit var database: AppDatabase
+    private var user:UserModel? = null
+    private lateinit var emailInput: EditText
+    private lateinit var passwordInput: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
 
         settingsActionBar()
 
+        database = AppDatabase.getDatabase(this)
+
         val loginhomedirect = findViewById<Button>(R.id.login2)
-        loginhomedirect.setOnClickListener{val intent = Intent(this,HomePage1::class.java)
-            startActivity(intent)}
+        emailInput = findViewById(R.id.loginEmailInput)
+        passwordInput = findViewById(R.id.loginPasswordInput)
+
+        loginhomedirect.setOnClickListener {
+            val email = emailInput.text.toString()
+            val password = passwordInput.text.toString()
+            // Acción asíncrona (corrutina) para hacer un registro en la BD
+
+            CoroutineScope(Dispatchers.IO).launch {
+                val result = async { database.users().login(email, password) }
+
+                if (result.await() != null) {
+                    goToHomeActivity()
+                } else {
+                    runOnUiThread {
+                        showErrorLoginToast()
+                    }
+                }
+            }
+
+            println("User in DB: $user")
+        }
     }
 
+    private fun goToHomeActivity() {
+        val intent = Intent(this,HomePage1::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_TASK_ON_HOME
+        startActivity(intent)
+        finish()
+    }
+
+    private fun showErrorLoginToast() {
+        Toast.makeText(this, "Usuario no encontrado, regístrate por favor", Toast.LENGTH_SHORT).show()
+    }
     // Configura el actionBar con un nuevo título y la posibilidad para poder de regresar al Main Activity.
     private fun settingsActionBar() {
         val actionBar = supportActionBar
